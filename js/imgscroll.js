05b09878fef6c34570f10467ca9018b8936809d3
Yoshida <sunnri257@gmail.com>
2025-06-22 16:11:17 +0900

    課題提出

---
+++ js/imgscroll.js
@@ -0,0 +1,17 @@
+// スクロール操作した時の処理を指定
+$(window).on('scroll', function () {
+  // クラス名【inview】が付与されている、全ての要素に対して処理を実行
+  $('.inview').each(function() {
+    // ブラウザの最上部から要素までの位置を取得
+    const targetPosition = $(this).offset().top;
+    // ブラウザの最上部からのスクロール量を取得
+    const scroll = $(window).scrollTop();
+    // ウィンドウの高さを取得
+    const windowHeight = $(window).height();
+    // ブラウザの最上部からのスクロール量 > 要素の位置 - ウィンドウの高さ の場合
+    if (scroll > targetPosition - windowHeight) {
+      // クラス名【show】を追加
+      $(this).addClass('show');
+    }
+  });
+});
