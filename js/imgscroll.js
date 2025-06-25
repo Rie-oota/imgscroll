c96529a2b3e09182322177b328cab210697809b2
Yoshida <sunnri257@gmail.com>
2025-06-25 23:21:28 +0900

    再提出

--- js/imgscroll.js
+++ js/imgscroll.js
@@ -1,7 +1,7 @@
 // スクロール操作した時の処理を指定
 $(window).on('scroll', function () {
-  // クラス名【inview】が付与されている、全ての要素に対して処理を実行
-  $('.inview').each(function() {
+  // クラス名【img_area】が付与されている、全ての要素に対して処理を実行
+  $('.img_area').each(function() {
     // ブラウザの最上部から要素までの位置を取得
     const targetPosition = $(this).offset().top;
     // ブラウザの最上部からのスクロール量を取得
