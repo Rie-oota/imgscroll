d8b64b79eb671aa33066826f72656281569233ac
Yoshida <sunnri257@gmail.com>
2025-06-26 21:26:45 +0900

    再修正

--- js/imgscroll.js
+++ js/imgscroll.js
@@ -1,15 +1,15 @@
 // スクロール操作した時の処理を指定
 $(window).on('scroll', function () {
-  // クラス名【img_area】が付与されている、全ての要素に対して処理を実行
-  $('.img_area').each(function() {
+  // クラス名【content】が付与されている、全ての要素に対して処理を実行
+  $('.content').each(function() {
     // ブラウザの最上部から要素までの位置を取得
     const targetPosition = $(this).offset().top;
     // ブラウザの最上部からのスクロール量を取得
     const scroll = $(window).scrollTop();
     // ウィンドウの高さを取得
     const windowHeight = $(window).height();
-    // ブラウザの最上部からのスクロール量 > 要素の位置 - ウィンドウの高さ の場合
-    if (scroll > targetPosition - windowHeight) {
+    // ブラウザの最上部からのスクロール量 > (要素の位置 - ウィンドウの高さ)+50 の場合
+    if (scroll > ( targetPosition - windowHeight ) + 50) {
       // クラス名【show】を追加
       $(this).addClass('show');
     }
